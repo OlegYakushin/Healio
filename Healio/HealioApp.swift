b61411c308591cad436334e8c7c61a50482af9ac
Oleg <yakushinoa@gmail.com>
2024-06-25 11:38:43 +0200

    Initial Commit

---
+++ Healio/HealioApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealioApp.swift
+//  Healio
+//
+//  Created by Oleg Yakushin on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct HealioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
